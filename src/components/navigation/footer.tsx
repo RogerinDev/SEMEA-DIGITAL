05f94890a74f126cdf6c23966745b6f2ed9c8c26
Rogério Otávio Filho <rogerinhootavio@gmail.com>
2025-06-11 12:21:19 +0000

    Poderia mudar essa parte dos direitos por favor? Quero tirar a "Secretar

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
   return (
     <footer className="border-t bg-background/80">
       <div className="container py-8 text-center text-sm text-muted-foreground">
-        <p>&copy; {currentYear} SEMEA Digital - Secretaria Municipal de Meio Ambiente de Varginha - MG.</p>
+        <p>&copy; {currentYear} SEMEA Digital - Centro Federal de Educação Tecnológica de Minas Gerais - Campus Varginha.</p>
         <p>Todos os direitos reservados.</p>
       </div>
     </footer>
