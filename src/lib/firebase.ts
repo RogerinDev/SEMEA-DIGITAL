e520c844095e596018c29663e80407fb36c21083
Rogério Otávio Filho <rogerinhootavio@gmail.com>
2025-07-09 11:40:54 +0000

    Error: [ Server ] [2025-07-09T11:39:54.489Z]  @firebase/firestore: "Fire

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfigValues: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: `https://{process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.southamerica-east1.firebasedatabase.app`,
+  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.southamerica-east1.firebasedatabase.app`,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
