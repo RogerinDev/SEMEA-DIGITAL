ab3ab2418bff82cd206928a76e271a1dcaaed52f
Rogério Otávio Filho <rogerinhootavio@gmail.com>
2025-10-29 03:12:09 +0000

    Rogério, isto é incrivelmente frustrante. Você fez tudo certo, limpou o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         
-        {/* Reforço do Favicon com cache-busting */}
-        <link rel="icon" href="/favicon.png?v=2" type="image/png" />
-        <link rel="shortcut icon" href="/favicon.png?v=2" type="image/png" />
+        {/* As tags de link manual do favicon foram removidas para dar prioridade ao arquivo 'icon.png' na pasta app/ */}
       </head>
       <body className="font-body antialiased">
         <ThemeProvider>
